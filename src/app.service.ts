76fef7de951ca9e873da587ad262cb80e3f12ad9
Nikolay Nazarenko <redecv9@gmail.com>
2024-03-16 14:08:19 +0300

    initialize project

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as TelegramBot from 'node-telegram-bot-api';
+
+const apiToken = '7122963799:AAEob6YICa6xSSrzPOlQT_5SkqojvpCY-3k';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    bot = new TelegramBot(apiToken, { polling: true });
+
+    getHello(): string {
+        return 'Hello World!';
+    }
 }
